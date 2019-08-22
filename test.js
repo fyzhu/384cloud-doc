6fddbef69ce729ba336159f11fe722ea8cce7caa
Viking Zhang <vikingmute@gmail.com>
2019-08-22 21:24:20 +0800

    return promise in QiniuManager async methods

--- test.js
+++ test.js
@@ -6,8 +6,13 @@ const localFile = "/Users/liusha/Desktop/name1.md";
 const key='name1.md';
 
 const manager = new QiniuManager(accessKey, secretKey, 'clouddoc')
-//manager.uploadFile(key, localFile)
-manager.deleteFile(key)
+manager.uploadFile(key, localFile).then((data) => {
+  console.log('上传成功',data)
+  return manager.deleteFile(key)
+}).then((data) => {
+  console.log('删除成功')
+})
+//manager.deleteFile(key)
 
 //const publicBucketDomain = 'http://pv8m1mqyk.bkt.clouddn.com';
 
