6fc985156686449387001f075c6abaea5bba0f9b
Viking Zhang <vikingmute@gmail.com>
2019-08-23 10:12:18 +0800

    add getBucketDomain and use it the generate download link

--- test.js
+++ test.js
@@ -6,13 +6,18 @@ const localFile = "/Users/liusha/Desktop/name1.md";
 const key='name1.md';
 
 const manager = new QiniuManager(accessKey, secretKey, 'clouddoc')
-manager.uploadFile(key, localFile).then((data) => {
-  console.log('上传成功',data)
-  return manager.deleteFile(key)
-}).then((data) => {
-  console.log('删除成功')
-})
+// manager.uploadFile(key, localFile).then((data) => {
+//   console.log('上传成功',data)
+//   return manager.deleteFile(key)
+// }).then((data) => {
+//   console.log('删除成功')
+// })
 //manager.deleteFile(key)
-
+manager.generateDownloadLink(key).then(data => {
+  console.log(data)
+  return manager.generateDownloadLink('first.md')
+}).then(data => {
+  console.log(data)
+})
 //const publicBucketDomain = 'http://pv8m1mqyk.bkt.clouddn.com';
 
