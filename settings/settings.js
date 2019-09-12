42bf6f81b9e0b70eff3e412401b7887371c9f12b
Viking Zhang <vikingmute@gmail.com>
2019-09-12 09:10:14 +0800

    11-1 add config qiniu section to settings.html, finish the tab feature in settings window

--- settings/settings.js
+++ settings/settings.js
@@ -2,28 +2,40 @@ const { remote } = require('electron')
 const Store = require('electron-store')
 const settingsStore = new Store({name: 'Settings'})
 
-const $ = (id) => {
-  return document.getElementById(id)
+const $ = (selector) => {
+  const result = document.querySelectorAll(selector)
+  return result.length > 1 ? result : result[0]
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   let savedLocation = settingsStore.get('savedFileLocation')
   if (savedLocation) {
-    $('saved-file-location').value = savedLocation
+    $('#savedFileLocation').value = savedLocation
   }
-  $('select-new-location').addEventListener('click', () => {
+  $('#select-new-location').addEventListener('click', () => {
     remote.dialog.showOpenDialog({
       properties: ['openDirectory'],
       message: '选择文件的存储路径',
     }, (path) => {
       if (Array.isArray(path)) {
-        $('saved-file-location').value = path[0]
+        $('#savedFileLocation').value = path[0]
         savedLocation = path[0]
       }
     })
   })
-  $('settings-form').addEventListener('submit', () => {
+  $('#settings-form').addEventListener('submit', () => {
     settingsStore.set('savedFileLocation', savedLocation)
     remote.getCurrentWindow().close()
+  })
+  $('.nav-tabs').addEventListener('click', (e) => {
+    e.preventDefault()
+    $('.nav-link').forEach(element => {
+      element.classList.remove('active')
+    })
+    e.target.classList.add('active')
+    $('.config-area').forEach(element => {
+      element.style.display = 'none'
+    })
+    $(e.target.dataset.tab).style.display = 'block'
   })
 })
