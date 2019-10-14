a7da87bbcc574f15e473a335b8ce8657b5b0b8bb
Viking Zhang <vikingmute@gmail.com>
2019-10-14 19:58:15 +0800

    add auto update part3

--- main.js
+++ main.js
@@ -17,11 +17,17 @@ const createManager = () => {
   return new QiniuManager(accessKey, secretKey, bucketName)
 }
 app.on('ready', () => {
+  if (isDev) {
+    autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml')
+  }
   autoUpdater.autoDownload = false
-  autoUpdater.checkForUpdatesAndNotify()
+  autoUpdater.checkForUpdates()
   autoUpdater.on('error', (error) => {
     dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
   })
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+  })  
   autoUpdater.on('update-available', () => {
     dialog.showMessageBox({
       type: 'info',
@@ -40,6 +46,22 @@ app.on('ready', () => {
       message: '当前已经是最新版本'
     })
   })
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    console.log(log_message)
+  })
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      title: '安装更新',
+      message: '更新下载完毕，应用将重启并进行安装'
+    }, () => {
+      setImmediate(() => autoUpdater.quitAndInstall())
+    })
+  })
+  
   const mainWindowConfig = {
     width: 1440,
     height: 768,   
